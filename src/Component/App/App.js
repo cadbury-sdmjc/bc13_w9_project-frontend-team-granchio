a993f93630a96d58ce5f1bf379bcf88bf9e1e350
C0dingChris <Chris.Sanger@outlook.com>
2022-11-23 16:15:36 +0000

    Styled landing page - CSS

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -2,6 +2,7 @@ import react, { useState } from "react";
 import "./App.css";
 import Button from "../Button/Button";
 import Forum from "../Forum/forum";
+import duckwithspeach from "./duckwithspeach.png"
 
 function App() {
   const [isShown, setIsShown] = useState(false);
@@ -12,12 +13,21 @@ function App() {
   return (
     <header className="App-header">
       <div className="App">
+      <img className="duck-img"alt = "Duck says How you feel today?" src={require('./duckwithspeach.png')}/>
         <div className="home-container">
           <div className="anon-duck-container">
-            <Button onClick={onClick} />
+            <div className="content-wrapper">
+              <h2 className="home-h2">CHEER ME UP</h2>
+              <p className="home-desc">Submit your mood to your personal duck to brighten your day</p>
+              <button className="angry-duck-btn">Angry Duck</button>
+            </div>
           </div>
           <div className="angry-duck-container">
-            <button className="angry-duck-btn">Angry Duck</button>
+          <div className="content-wrapper">
+            <h2 className="home-h2">SUPPORT EACH OTHER</h2>
+            <p className="home-desc">Talk anonymously to your fellow bootcampers and share struggles</p>
+            <Button onClick={onClick} />
+            </div>
           </div>
         </div>
         {isShown && <Forum></Forum>}
